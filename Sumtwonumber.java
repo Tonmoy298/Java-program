0e38134349416ce3ec2b16dc7ae05588437b70ee
Tonmoy298 <tonmoyofs@gmail.com>
2026-02-13 01:27:33 +0600

    Add files via upload

---
+++ Sumtwonumber.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.sumtwonumber;
+
+
+public class Sumtwonumber {
+
+    public static void main(String[] args) {
+        
+        int x=3,y=7;
+        int sum=x+y;
+        
+        System.out.println("Sum is:"+sum);
+    }
+}
