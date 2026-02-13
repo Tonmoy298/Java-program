08137ddd3b4e17b617c70fccf235e1622a3b8fc9
Tonmoy298 <tonmoyofs@gmail.com>
2026-02-14 01:52:58 +0600

    Add files via upload

---
+++ newob.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.object;
+
+
+
+public class newob {
+   
+ double w,h;
+    public static void main(String[] args) {
+        
+     newob  obj=new newob () ;  
+        obj.h=4.2;
+        obj.w=20.1;
+        System.out.println("The height is:"+obj.h);
+        
+        System.out.println("The weight is:"+obj.w);
+    }
+}
