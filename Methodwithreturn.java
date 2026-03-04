8415dd4035efe8d994b414a0f7892df6666d3b78
Tonmoy298 <tonmoyofs@gmail.com>
2026-03-05 03:10:38 +0600

    Add files via upload

---
+++ Methodwithreturn.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.methodwithreturn;
+
+
+public class Methodwithreturn {
+
+ public static int sum(int a,int b)  {
+
+  return a+b;
+ }
+    public static void main(String[] args) {
+        
+       int result=sum(3,1);
+        System.out.println("The sum is:"+result);
+    }
+}
