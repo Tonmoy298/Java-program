7027c2fe55c590c7a8cbe2627f2b487c32c66a3f
Tonmoy298 <tonmoyofs@gmail.com>
2026-03-06 22:51:32 +0600

    Add files via upload

---
+++ Methodwithoutreturntype.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.methodwithoutreturntype;
+
+
+public class Methodwithoutreturntype {
+
+   public static void sum (int a,int b){
+     int result=a+b;  
+       
+       System.out.println("The Addition is:"+result);
+   }
+    
+    
+    public static void main(String[] args) {
+        
+       sum(4,7) ;
+        
+       
+    }
+}
